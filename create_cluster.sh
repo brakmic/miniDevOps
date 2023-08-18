af5d03f1b2b1c9d16eaa3b103d3a5ce005a94a69
Harris Brakmić <brakmic@users.noreply.github.com>
2023-08-18 07:58:26 +0200

    Update create_cluster.sh

--- create_cluster.sh
+++ create_cluster.sh
@@ -7,4 +7,4 @@ echo " --- Deploying NGINX IngressController ---"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 echo " --- Waiting for deployment to complete ---"
 kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s
-echo " --- K8S is now ready --- "
+echo " --- k8s is now ready --- "
