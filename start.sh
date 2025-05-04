ed767d8190022141b5c62db94e9cd2a87b91bc6e
Harris Brakmic <brakmic@gmail.com>
2025-05-04 15:21:09 +0200

    fix: permissions on docker.sock

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Fix Docker socket permissions if it exists
+if [ -e "/var/run/docker.sock" ]; then
+    sudo chmod 666 /var/run/docker.sock
+fi
+
 # Write or export environment variables to the user's .bashrc
 echo 'export PIPENV_IGNORE_VIRTUALENVS=1' >> "$HOME/.bashrc"
 
